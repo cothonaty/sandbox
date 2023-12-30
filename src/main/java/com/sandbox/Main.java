77f6352cb6a46cbea2db6a3916696a45c346a9bd
cothonaty <154710556+cothonaty@users.noreply.github.com>
2023-12-30 21:47:26 +0400

    Update Main.java

--- src/main/java/com/sandbox/Main.java
+++ src/main/java/com/sandbox/Main.java
@@ -4,38 +4,43 @@ import java.util.Arrays;
 
 class Solution {
 
-    int[] d = {1, 2, 2, 3, 3, 2, 1};
+    int[] d = {1, 3,2, 2,2, 3, 3, 2, 1};
+
 
     public int removeDuplicates(int[] nums) {
         int uniqueElement = 0;
-        int b = 0;
-        int position = 0;
-        int element = 0;
         for (int i = 0; i < nums.length; i++) {
-
             uniqueElement = nums[i];
-            if (uniqueElement == -1) continue;
             int k = i + 1;
-
             //заменяем всех кроме предыщего, так как он уникальный
             for (int q = k; q < nums.length; q++) {
-
                 if (nums[q] == uniqueElement) {
                     nums[q] = -1;
-                    int temp = nums[nums.length-2];
-                    nums[nums.length-1] = nums[q];
-                    nums[q] = temp;
-                } else {
-                    position = q;
-                    element = nums[q];
-                    nums[position] = element;
+                }
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == -1) {
+                for (int q = i + 1; q < nums.length; q++) {
+                    if (nums[q] != -1) {
+                        nums[i] = nums[q];
+                        nums[q] = -1;
+                    }
                 }
             }
 
         }
-
-
-        return 1; // Возвращаем количество уникальных элементов
+        int result=0;
+        for (int i =0; i<nums.length;i++){
+           if(nums[i] ==-1){
+               result = i;
+               break;
+           }
+        }
+            return result;
+         // Возвращаем количество уникальных элементов
     }
 }
 
@@ -43,7 +48,8 @@ class Solution {
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.removeDuplicates(s.d);
+        System.out.println(s.removeDuplicates(s.d));
         Arrays.stream(s.d).forEach(System.out::print);// Выводим результат
+
     }
 }
